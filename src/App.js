99643e77a688fcd32cd26175eb0ad0b5ce78e3db
roberto <robpalacios1@hotmail.com>
2021-01-27 14:38:28 -0500

    import header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
